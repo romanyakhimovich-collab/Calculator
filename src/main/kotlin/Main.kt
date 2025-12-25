cf6ff8750dba187ddb8d5bd7b8630a018898035d
roman <roman.yakhimovich@gmail.com>
2025-12-25 22:33:35 +0200

    Changed the logic of if to make the code more clear

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,17 +2,19 @@ fun main() {
     val num1 = readln().toDouble()
     val sign = readln()
     val num2 = readln().toDouble()
-    if (sign == "/") {
-        if (num2 == 0.0) {
-            println("Error: cannot divide by zero!")
-        }
+    if (sign == "/" && num2 == 0.0) {
+        println("Error: cannot divide by zero!")
+        return
     }
     val res = when (sign) {
         "+" -> num1 + num2
         "-" -> num1 - num2
         "*" -> num1 * num2
         "/" -> num1 / num2
-        else -> "Error: unknown sign"
+        else -> {
+            println("Error: unknown sign")
+            return
+        }
     }
     println(res)
 }
