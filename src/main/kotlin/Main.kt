863c994f2a120f80102b7cac42b5a191da440a1b
roman <roman.yakhimovich@gmail.com>
2025-12-25 22:41:36 +0200

    Add single-line expression input

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,14 @@
 fun main() {
-    val num1 = readln().toDouble()
-    val sign = readln()
-    val num2 = readln().toDouble()
+    println("Enter the expression (e.g. 2+3):")
+    val input = readln()
+    val parts = input.split(" ")
+    if (parts.size != 3) {
+        println("Error: invalid expression")
+        return
+    }
+    val num1 = parts[0].toDouble()
+    val sign = parts[1]
+    val num2 = parts[2].toDouble()
     if (sign == "/" && num2 == 0.0) {
         println("Error: cannot divide by zero!")
         return
@@ -16,5 +23,5 @@ fun main() {
             return
         }
     }
-    println(res)
+    println("Result: $res")
 }
