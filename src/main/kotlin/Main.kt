3747628743a305496b546e41166357bf50b1d47c
roman <roman.yakhimovich@gmail.com>
2025-12-25 22:57:41 +0200

    Support expressions without spaces

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,24 @@
 fun main() {
-    println("Enter the expression (e.g. 2+3):")
-    val input = readln()
-    val parts = input.split(" ")
-    if (parts.size != 3) {
+    println("Enter the expression (e.g. 2+3 or 2 + 3):")
+    val input = readln().replace(" ", "")
+    val operators = listOf("+", "-", "*", "/")
+    val operator = operators.firstOrNull() {it in input}
+    if (operator == null) {
+        println("Error: no operator found in expression")
+        return
+    }
+    val parts = input.split(operator)
+    if (parts.size != 2) {
         println("Error: invalid expression")
         return
     }
     val num1 = parts[0].toDouble()
-    val sign = parts[1]
-    val num2 = parts[2].toDouble()
-    if (sign == "/" && num2 == 0.0) {
+    val num2 = parts[1].toDouble()
+    if (operator == "/" && num2 == 0.0) {
         println("Error: cannot divide by zero!")
         return
     }
-    val res = when (sign) {
+    val res = when (operator) {
         "+" -> num1 + num2
         "-" -> num1 - num2
         "*" -> num1 * num2
